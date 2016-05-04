f6bc4f98f2e600d3029d5e160244d283d5ba070d
harsuu <harsh.vyomwork@gmail.com>
2016-05-04 19:17:12 +0530

    Database Create

---
+++ DBHelper.java
@@ -0,0 +1,28 @@
+package com.vrs.employeeapp;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+public class DBHelper extends SQLiteOpenHelper {
+	static final String DATABASE = "empapp.db";
+	static final int VERSION = 1;
+	static final String TABLE = "emp";
+	static final String C_ID = "_id";
+	static final String C_ENAME = "ename";
+	static final String C_DESIGNATION = "designation";
+	static final String C_SALARY = "salary";
+	public DBHelper(Context context) {
+		super(context, DATABASE, null, VERSION);
+
+	}
+	@Override
+	public void onCreate(SQLiteDatabase db){
+		db.execSQL("CREATE TABLE " + TABLE + " ( " + C_ID
+				+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + C_ENAME + " text, "
+				+ C_DESIGNATION + " text, " + C_SALARY + " text )");
+	}
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("Drop table " + TABLE);
+		onCreate(db);
+	}
+}
